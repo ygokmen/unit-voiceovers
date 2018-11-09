263b11a535236d448e5ad647513287146d8a5fe9
SceptreOfficial <invictix@gmail.com>
2018-11-08 17:33:20 -0700

    Go back to using wav format on death shouts

--- addons/UVO/sounds/soundsCommon.hpp
+++ addons/UVO/sounds/soundsCommon.hpp
@@ -1,312 +1,312 @@
 class UVO_deathShout01
 {
 	name = "UVO_deathShout01";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout01.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout01.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout02
 {
 	name = "UVO_deathShout02";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout02.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout02.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout03
 {
 	name = "UVO_deathShout03";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout03.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout03.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout04
 {
 	name = "UVO_deathShout04";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout04.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout04.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout05
 {
 	name = "UVO_deathShout05";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout05.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout05.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout06
 {
 	name = "UVO_deathShout06";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout06.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout06.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout07
 {
 	name = "UVO_deathShout07";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout07.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout07.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout08
 {
 	name = "UVO_deathShout08";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout08.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout08.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout09
 {
 	name = "UVO_deathShout09";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout09.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout09.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout10
 {
 	name = "UVO_deathShout10";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout10.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout10.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout11
 {
 	name = "UVO_deathShout11";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout11.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout11.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout12
 {
 	name = "UVO_deathShout12";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout12.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout12.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout13
 {
 	name = "UVO_deathShout13";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout13.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout13.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout14
 {
 	name = "UVO_deathShout14";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout14.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout14.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout15
 {
 	name = "UVO_deathShout15";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout15.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout15.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout16
 {
 	name = "UVO_deathShout16";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout16.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout16.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout17
 {
 	name = "UVO_deathShout17";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout17.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout17.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout18
 {
 	name = "UVO_deathShout18";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout18.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout18.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout19
 {
 	name = "UVO_deathShout19";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout19.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout19.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout20
 {
 	name = "UVO_deathShout20";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout20.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout20.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout21
 {
 	name = "UVO_deathShout21";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout21.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout21.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout22
 {
 	name = "UVO_deathShout22";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout22.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout22.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout23
 {
 	name = "UVO_deathShout23";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout23.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout23.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout24
 {
 	name = "UVO_deathShout24";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout24.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout24.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout25
 {
 	name = "UVO_deathShout25";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout25.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout25.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout26
 {
 	name = "UVO_deathShout26";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout26.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout26.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout27
 {
 	name = "UVO_deathShout27";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout27.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout27.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout28
 {
 	name = "UVO_deathShout28";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout28.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout28.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout29
 {
 	name = "UVO_deathShout29";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout29.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout29.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout30
 {
 	name = "UVO_deathShout30";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout30.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout30.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout31
 {
 	name = "UVO_deathShout31";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout31.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout31.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout32
 {
 	name = "UVO_deathShout32";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout32.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout32.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout33
 {
 	name = "UVO_deathShout33";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout33.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout33.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout34
 {
 	name = "UVO_deathShout34";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout34.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout34.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout35
 {
 	name = "UVO_deathShout35";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout35.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout35.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout36
 {
 	name = "UVO_deathShout36";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout36.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout36.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout37
 {
 	name = "UVO_deathShout37";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout37.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout37.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout38
 {
 	name = "UVO_deathShout38";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout38.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout38.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout39
 {
 	name = "UVO_deathShout39";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout39.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout39.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout40
 {
 	name = "UVO_deathShout40";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout40.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout40.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout41
 {
 	name = "UVO_deathShout41";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout41.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout41.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout42
 {
 	name = "UVO_deathShout42";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout42.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout42.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout43
 {
 	name = "UVO_deathShout43";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout43.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout43.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout44
 {
 	name = "UVO_deathShout44";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout44.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout44.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout45
 {
 	name = "UVO_deathShout45";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout45.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout45.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout46
 {
 	name = "UVO_deathShout46";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout46.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout46.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout47
 {
 	name = "UVO_deathShout47";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout47.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout47.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout48
 {
 	name = "UVO_deathShout48";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout48.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout48.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout49
 {
 	name = "UVO_deathShout49";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout49.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout49.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout50
 {
 	name = "UVO_deathShout50";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout50.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout50.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout51
 {
 	name = "UVO_deathShout51";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout51.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout51.wav",2.6,1,100};
 	titles[] = {};
 };
 class UVO_deathShout52
 {
 	name = "UVO_deathShout52";
-	sound[] = {"UVO\sounds\common\deathShout\deathShout52.ogg",2.6,1,100};
+	sound[] = {"UVO\sounds\common\deathShout\deathShout52.wav",2.6,1,100};
 	titles[] = {};
 };
